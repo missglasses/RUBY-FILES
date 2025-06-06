f197295c75a9b24c57b73b55d325fdf2930e9c0a
Cindy Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-06 13:19:55 +0800

    Create Ruby-Prompts.rb

---
+++ Ruby-Prompts.rb
@@ -0,0 +1,30 @@
+#Prompts
+
+first_name = gets.chomp #user input
+
+#Practice
+print "First name: "
+first_name = gets.chomp
+
+print "Last name: "
+last_name = gets.chomp
+print last_name, "\n"
+
+print "City: (abbr.) "
+city = gets.chomp
+puts city.upcase
+
+print "State: "
+state = gets.chomp
+print state
+
+#You can also do this
+name= "vamfayr"
+
+puts "i know, # {name} right!" #i know, vamfayr right?
+
+#.capitalize! -- capitalizes first letter
+name.capitalize! #I know, vamfayr right?
+
+
+
