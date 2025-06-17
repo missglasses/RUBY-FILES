511a1596968b0a3c24cbb4f5a2f8267511141013
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-18 00:33:49 +0800

    Update Ruby-Arrays.rb

--- Ruby-Arrays.rb
+++ Ruby-Arrays.rb
@@ -8,8 +8,40 @@ string_array = ["w", "h", "a", "t"]
 
 #MultiD
 multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+#or
+
+my_array = [
+  [1, 2, 3],
+  ["tulips", "lilies", "daisies"],
+  [true, false, true],
+  ["cloud", 7, 3.14, false]
+]
 
 multi_d_array.each { |x| puts "#{x}\n" }
 
 # 2D
 my_2d_array =[[1,3,5],[2,4,6]]
+
+#1D iteration
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each { |lang| puts lang }
+
+#multiD iteration
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+s.each do |sub_array|
+  sub_array.each do |item|
+    puts item
+  end
+end
+
+#hashes iteration
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each { |name, food| puts food }
