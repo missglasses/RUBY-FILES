81afca0dca45dbd2d4b324d4261821cd8c1ddd43
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-18 00:20:14 +0800

    Create Ruby-Arrays.rb

---
+++ Ruby-Arrays.rb
@@ -0,0 +1,15 @@
+#Arrays in Ruby
+
+my_array=[1,2,3,4,5]
+print my_array[2] #3
+
+#String
+string_array = ["w", "h", "a", "t"]
+
+#MultiD
+multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+multi_d_array.each { |x| puts "#{x}\n" }
+
+# 2D
+my_2d_array =[[1,3,5],[2,4,6]]
